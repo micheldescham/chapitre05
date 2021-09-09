83881601ab1f3689e25390371a4ad72552d20fe1
mciheldescham <michel.descham@gmail.com>
2021-09-09 11:01:57 -0400

    correction de fautes

--- os_demo.js
+++ os_demo.js
@@ -5,4 +5,4 @@ console.log(os.arch());
 console.log(os.cpus());
 console.log('Mémoire dispo: (Go) ', os.freemem()/1024/1024/1024);
 console.log('Mémoire totale: (Go) ', os.totalmem()/1024/1024/1024);
-console.log('Uptime: (en heures) ', os.uptime()/60/60); //combien de tmeps en secondes
+console.log('Uptime: (en heures) ', os.uptime()/60/60); //combien de temps en secondes
